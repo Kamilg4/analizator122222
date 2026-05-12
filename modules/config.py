191a2b822e6193423f0783046e9428fd87376bde
Kamilg4 <107170130+Kamilg4@users.noreply.github.com>
2026-05-12 22:02:26 +0200

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,5 +7,5 @@ FIBO_LEVELS = [0.382, 0.414, 0.5, 0.618, 0.667]
 FIBO_MAIN_LEVELS = [0.5, 0.618, 0.667]
 DEFAULT_RSI_PERIOD = 14
 DEFAULT_ATR_PERIOD = 14
-APP_VERSION = "2026-05-12-ranking-v4 (Zmodularyzowana)"
+APP_VERSION = "2026-05-12-strefy-v5-rzetelnosc-i-czytelnosc"
 TOP_ZONES_TO_DISPLAY = 5
